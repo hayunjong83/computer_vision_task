84373ceda4e6035b3f18b6dc380c826bdc0494ad
hayunjong83 <hayunjong83@gmail.com>
2022-07-13 11:26:53 +0900

    upload trial file

---
+++ OpenCV/1_basic_operation/image_read_trial.py
@@ -0,0 +1,16 @@
+# OpenCV 라이브러리를 로드
+
+def main():
+    # 'kei.png' 이미지를 grayscale로 읽어온다.
+
+    # 읽은 이미지의 데이터타입과 형태를 확인한다.
+
+    # 읽은 이미지를 화면에 띄운다.
+
+    # 이미지를 'save_img.jpg'로 저장한다.
+
+    pass
+
+if __name__ == '__main__':
+    main()
+    
